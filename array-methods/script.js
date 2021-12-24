5c0c810e3698a38f442b056350fec06aa195c611
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-12-24 10:06:01 +0800

    Output the users using foreach method and DOM methods

--- array-methods/script.js
+++ array-methods/script.js
@@ -16,7 +16,7 @@ async function getRandomUser() {
   const res = await fetch('https://randomuser.me/api')
   const data = await res.json()
 
-  console.log(data)
+  // console.log(data)
   const user = data.results[0]
 
   const newUser = {
@@ -30,4 +30,29 @@ async function getRandomUser() {
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj)
+
+  updateDOM()
 }
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>'
+
+  providedData.forEach((item) => {
+    const element = document.createElement('div')
+    element.classList.add('person')
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`
+    main.appendChild(element)
+  })
+}
+
+// Format number as money -- https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-strings
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
+}
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser)
