f3ebad681670299ad579d28650357570db350602
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-12-27 12:49:16 +0800

    Minor refactor in index.html -- Add a filter method to show the millionaire

--- array-methods/script.js
+++ array-methods/script.js
@@ -1,7 +1,7 @@
 const main = document.getElementById('main')
 const addUserBtn = document.getElementById('add-user')
 const doubleBtn = document.getElementById('double')
-const showMillionaires = document.getElementById('show-millionaires')
+const showMillionairesBtn = document.getElementById('show-millionaires')
 const sortBtn = document.getElementById('sort')
 const calculateWealthBtn = document.getElementById('calculate-wealth')
 
@@ -43,6 +43,13 @@ function sortByRichest() {
   updateDOM()
 }
 
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter((user) => user.money > 1000000)
+
+  updateDOM()
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj)
@@ -74,3 +81,4 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser)
 doubleBtn.addEventListener('click', doubleMoney)
 sortBtn.addEventListener('click', sortByRichest)
+showMillionairesBtn.addEventListener('click', showMillionaires)
